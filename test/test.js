74e9eb185d52cdca1993a1951db3cb1565fe3c25
Cosmin Popovici <hellocosmin@gmail.com>
2023-02-15 14:37:20 +0200

    build: fix linting errors

--- test/test.js
+++ test/test.js
@@ -1,9 +1,8 @@
-const test = require('ava')
-const plugin = require('../lib')
-const posthtml = require('posthtml')
-
 const path = require('path')
 const {readFileSync} = require('fs')
+const test = require('ava')
+const posthtml = require('posthtml')
+const plugin = require('../lib/index.js')
 
 const fixture = file => readFileSync(path.join(__dirname, 'fixtures', `${file}.html`), 'utf8')
 const expected = file => readFileSync(path.join(__dirname, 'expected', `${file}.html`), 'utf8')
