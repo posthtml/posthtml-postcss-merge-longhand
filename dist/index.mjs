a966efafd68879936eeddfc03d58ac4193f4a8d1
Cosmin Popovici <hellocosmin@gmail.com>
2024-04-19 18:05:28 +0300

    feat: support both esm and cjs

---
+++ dist/index.mjs
@@ -0,0 +1,21 @@
+import postcss from 'postcss';
+import safe from 'postcss-safe-parser';
+import mergeLonghand from 'postcss-merge-longhand';
+
+const plugin = (options = {}) => (tree) => {
+  options.tags = options.tags || [];
+  const process = (node) => {
+    const tags = new Set(options.tags);
+    if (tags.size > 0 && !tags.has(node.tag)) {
+      return node;
+    }
+    if (node.attrs && node.attrs.style) {
+      const { css } = postcss().use(mergeLonghand).process(`div { ${node.attrs.style} }`, { parser: safe });
+      node.attrs.style = css.replace(/div {\s|\s}$/gm, "");
+    }
+    return node;
+  };
+  return tree.walk(process);
+};
+
+export { plugin as default };
